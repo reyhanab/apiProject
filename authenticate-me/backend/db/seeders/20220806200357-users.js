fec011a2d5f7d905c0dece2c611cadbeeead3ff0
Reyhaneh <Reyhaneh.Abdollahi2@gmail.com>
2022-08-06 17:15:03 -0400

    Initialize Express and Sequelize with CSRF protection

---
+++ authenticate-me/backend/db/seeders/20220806200357-users.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    // await queryInterface.bulkInsert('Users', users)
+  },
+
+  async down (queryInterface, Sequelize) {
+
+  }
+};
